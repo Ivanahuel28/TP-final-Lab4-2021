c99e989688ad2189b3a14a41f75cd2d1b33729b2
Ivan <ivanahuel.ing@gmail.com>
2021-10-12 12:56:57 -0300

    Primer commit: Framework base

---
+++ Views/home.php
@@ -0,0 +1,10 @@
+<?php
+require_once('nav.php');
+?>
+<main class="py-5">
+	<section id="listado" class="mb-5">
+		<div class="container">
+			<h2 class="mb-4">Bienvenido</h2>
+		</div>
+	</section>
+</main>
