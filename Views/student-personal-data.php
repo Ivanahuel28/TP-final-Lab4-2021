45abb9b9ea40e4a79e05a07ff318c2698c197bc6
Ivan Gomez <ivanahuel.ing@gmail.com>
2021-10-22 18:56:07 -0300

    1er entrega metodologia 22/10

--- Views/student-personal-data.php
+++ Views/student-personal-data.php
@@ -16,6 +16,14 @@ require_once('student-nav.php');
 				<label for="formGroupExampleInput2" class="form-label">Apellido</label>
 				<input type="text" name="role" value="<?php echo $student->getLastname() ?>" class="form-control" id="formGroupExampleInput2" readonly>
 			</div>
+			<div class="mb-3">
+				<label for="formGroupExampleInput2" class="form-label">Dni</label>
+				<input type="text" name="role" value="<?php echo $student->getDni() ?>" class="form-control" id="formGroupExampleInput2" readonly>
+			</div>
+			<div class="mb-3">
+				<label for="formGroupExampleInput2" class="form-label">Correo</label>
+				<input type="text" name="role" value="<?php echo $student->getEmail() ?>" class="form-control" id="formGroupExampleInput2" readonly>
+			</div>
 			<div class="form-group d-flex justify-content-end">
 				<div>
 					<a type="submit" href="<?php echo FRONT_ROOT ?>Home/GoToStudentHome" class="btn btn-secondary m-2 p-auto">Volver</a>
